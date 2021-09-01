fc343ed2712fb0108533255c420b5a9e9f3c34c0
yuki4201 <youkikoi@icloud.com>
2021-09-01 07:10:37 +0000

    Initial Commit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -8,7 +8,7 @@ class Admin::GenresController < ApplicationController
   def create
     @genre = Genre.new(genre_params)
     @genre.save
-    redirect_to admin_items_path
+    redirect_to admin_genres_path
   end
   
   def edit
@@ -17,6 +17,8 @@ class Admin::GenresController < ApplicationController
   
   def update
     @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
   
   private
